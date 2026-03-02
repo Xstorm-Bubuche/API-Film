da435e3c018bebb3dc8f75e00c617a8a770517af
Bubuche <guilhem.mandallena@ynov.com>
2026-03-02 21:14:20 +0100

    V2+com

--- index.php
+++ index.php
@@ -1,32 +1,58 @@
 <?php
+require_once __DIR__ . '/config/config.php';
+require_once __DIR__ . '/controllers/MovieController.php';
+require_once __DIR__ . '/controllers/FavoriteControllers.php';
 
-require_once 'config/config.php';
-require_once 'controllers/MovieController.php';
 
-header("Content-Type: application/json");
-
+$requestUri = $_SERVER['REQUEST_URI'];
+$path = strtok($requestUri, '?');
 $method = $_SERVER['REQUEST_METHOD'];
-$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if ($path === '/movies' && $method === 'GET') {
-
+// routing
+if ($method === 'GET' && preg_match('#^/movies$#', $path)) {
     $type = $_GET['type'] ?? 'popular';
     MovieController::list($type);
 
-}
-elseif ($path === '/favorites' && $method === 'POST') {
+// recherche
+} elseif ($method === 'GET' && preg_match('#^/movies/search$#', $path)) {
+    $query = $_GET['q'] ?? '';
+    MovieController::search($query);
 
-    MovieController::addFavorite();
+// detail
+} elseif ($method === 'GET' && preg_match('#^/movies/(\d+)$#', $path, $matches)) {
+    MovieController::detail((int)$matches[1]);
 
-}
-elseif ($path === '/favorites' && $method === 'GET') {
+// get favoris
+} elseif ($method === 'GET' && preg_match('#^/favorites$#', $path)) {
+    FavoriteController::getAll();
 
-    MovieController::getFavorites();
+// post favoris
+} elseif ($method === 'POST' && preg_match('#^/favorites$#', $path)) {
+    FavoriteController::add();
 
-}
-else {
+// del favoris
+} elseif ($method === 'DELETE' && preg_match('#^/favorites/(\d+)$#', $path, $matches)) {
+    FavoriteController::remove((int)$matches[1]);
 
+// accueil
+} elseif ($method === 'GET' && $path === '/') {
+    echo json_encode([
+        'api'     => 'API Films',
+        'version' => '1.0',
+        'routes'  => [
+            'GET /movies?type=popular'    => 'Films populaires',
+            'GET /movies?type=top_rated'  => 'Meilleurs films',
+            'GET /movies?type=upcoming'   => 'Prochainement',
+            'GET /movies?type=now_playing'=> 'En ce moment',
+            'GET /movies/search?q=titre'  => 'Recherche',
+            'GET /movies/{id}'            => 'Détail film',
+            'GET /favorites'              => 'Liste favoris',
+            'POST /favorites'             => 'Ajouter favori',
+            'DELETE /favorites/{id}'      => 'Supprimer favori',
+        ]
+    ], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+// erreur
+} else {
     http_response_code(404);
-    echo json_encode(["error" => "Route inconnue"]);
-
+    echo json_encode(['error' => 'Route inconnue', 'path' => $path, 'method' => $method]);
 }
